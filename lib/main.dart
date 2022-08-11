f524ce359d2eae1868e88947037b55009023384e
bello1111 <belal.bassem123@gmail.com>
2022-08-11 08:55:12 +0200

    added splash

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'providers/users.dart';
 import 'screens/user_profile_screen.dart';
 import 'screens/users_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +23,11 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'GT Certificate',
         theme: ThemeData(primarySwatch: Colors.teal),
-        home: const UsersScreen(),
+        home: AnimatedSplashScreen(
+            nextScreen: const UsersScreen(),
+            splash: Image.asset("assets/logo.png"),
+            duration: 2500,
+            backgroundColor: Color.fromRGBO(20, 255, 231, 0.498)),
         routes: {
           UserProfileScreen.routeName: (ctx) => const UserProfileScreen(),
         },
